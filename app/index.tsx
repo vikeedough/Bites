5715eb6afc33ac62e9963cfecf7c8a01f5b5e600
vikeedough <d.vicedo06@gmail.com>
2024-05-25 14:13:50 +0800

    250524

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen hh.</Text>
     </View>
   );
 }
