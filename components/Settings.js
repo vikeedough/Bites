381925d7b8a9f4200f6abf404f7c2ddbefd680b0
StJ0nas <e1122931@u.nus.edu>
2024-06-02 12:25:50 +0800

    Update Settings.js
    Updated Settings Page

--- components/Settings.js
+++ components/Settings.js
@@ -1,22 +1,52 @@
 import React from 'react';
-import { Text, View, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
 import {firebaseApp, firebaseAuth} from '../firebaseConfig'
 
 const app = firebaseApp
 const auth = firebaseAuth
 
 export default function Settings() {
+  return (
+    <ScrollView contentContainerStyle={{flexGrow: 1}}>
 
-    return (
-        <View style = {styles.container}>
-          <TouchableOpacity>
-            <Text style = {{color: 'blue', margin: 10}} onPress = {() => auth.signOut()}>Sign out</Text>
-          </TouchableOpacity>
+        <View style={styles.ButtonContainer}>
+            <TouchableOpacity>
+                <Text style={styles.ButtonText}>Profile</Text>
+            </TouchableOpacity>
         </View>
-      );
+
+        <View style={styles.ButtonContainer}>
+            <TouchableOpacity>
+                <Text style={styles.ButtonText}>Goals</Text>
+            </TouchableOpacity>
+        </View>
+
+        <View style={styles.ButtonContainer}>
+            <TouchableOpacity>
+                <Text style={styles.ButtonText}>Progress</Text>
+            </TouchableOpacity>
+        </View>
+        
+        <View style={styles.ButtonContainer}>
+            <TouchableOpacity onPress = {() => auth.signOut()}>
+                <Text style={styles.ButtonText}>Logout</Text>
+            </TouchableOpacity>
+        </View>
+      </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
+  ButtonContainer: {
+    flex: 0.08,
+    justifyContent: 'center',
+    alignItems: 'left'
+  },
+  ButtonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingLeft: 20
+  },
   container: { 
     flex: 1,
     justifyContent: 'center', 
