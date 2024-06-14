766aca6ff7e0cc9a9627e12ae8822b94bccb9a41
vikeedough <d.vicedo06@gmail.com>
2024-06-15 02:10:36 +0800

    finished Post.js

--- components/Post.js
+++ components/Post.js
@@ -1,26 +1,86 @@
 import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, Touchable } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { useState } from 'react';
+import { useState, React } from 'react';
+import { firebaseApp, firebaseAuth, firebaseDb } from '@/firebaseConfig';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import * as ImagePicker from 'expo-image-picker';
+import { updateProfile } from 'firebase/auth';
+import { arrayUnion, doc, setDoc, updateDoc } from 'firebase/firestore';
 
+const app = firebaseApp;
+const auth = firebaseAuth;
+const db = firebaseDb;
 const placeholder = require('@/assets/images/placeholder.png');
+const storage = getStorage();
 
 export default function Post() {
 
     const [image, setImage] = useState(null);
-    
+    const [caption, setCaption] = useState('');
+    const [picture, setPicture] = useState(null);
+    const user = auth.currentUser;
+
+    const uriToBlob = async (uri) => {
+        const response = await fetch(uri);
+        const blob = await response.blob();
+        return blob;
+      };
+
+    const addImage = async () => {
+        let picture = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1,1],
+            quality: 1,
+          });
+        setPicture(picture);
+        if (!picture.canceled) {
+            setImage(picture.assets[0].uri);
+        }
+    }
+
+    const sendPost = async () => {
+        const blob = await uriToBlob(picture.assets[0].uri);
+        let timeNow = Date.now();
+        console.log(timeNow);
+        let pictureRef = ref(storage, user.uid + '/posts/' + timeNow + '.jpeg');
+        await uploadBytes(pictureRef, blob).then((snapshot) => {
+            console.log('Uploaded picture onto Storage!');
+        });
+        let url = await getDownloadURL(pictureRef);
+        const postObject = {
+            pictureURL: url,
+            caption: caption,
+            time: timeNow
+        }
+        const postsRef = doc(db, "users", user.uid);
+        await updateDoc(postsRef, {
+            posts: arrayUnion(postObject)
+        }).then(() => {
+            setImage(null);
+            setCaption('');
+            setPicture(null);
+            alert('Post successfully uploaded!')
+        });
+    }
 
     return(
         <View style={styles.container}>
             <View style={styles.captionContainer}>
                 <Text style={styles.captionText}>Caption</Text>
             </View>
-            <TextInput style={styles.description} placeholder='Add a description!' multiline />
-            <TouchableOpacity style={styles.imageUploadContainer}>
+            <TextInput 
+                style={styles.description} 
+                placeholder='Add a description!'
+                onChangeText={setCaption}
+                value={caption} 
+                multiline />
+            <TouchableOpacity style={styles.imageUploadContainer} onPress={addImage}>
                 {
                     image === null ? 
                         <Text>Add Image</Text>
                         :
-                        <Image resizeMode='auto' source={placeholder} style={Styles.uploadedImage} />
+                        <Image source={{uri: image}} style={styles.uploadedImage} />
                     
                 }
             </TouchableOpacity>
@@ -31,7 +91,7 @@ export default function Post() {
                     <Ionicons name="chevron-forward" color={"grey"} size={18} />
                 </TouchableOpacity>
             </View>
-            <TouchableOpacity style={styles.postButton}>
+            <TouchableOpacity style={styles.postButton} onPress={sendPost}>
                 <Text style={styles.postButtonText}>Post</Text>
             </TouchableOpacity>
         </View>
@@ -75,11 +135,10 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         aspectRatio: 1,
-        padding: 60,
     },
     uploadedImage: {
         position: 'relative',
-        width: 34,
+        width: '100%',
         margin: 30,
         aspectRatio: 1,
     },
